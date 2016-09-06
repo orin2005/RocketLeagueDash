834a2e8631e578077bed8fb7c9a7d62930a3fce6
developer <developer>
2016-09-06 12:06:53 -0500

    added KeyboardManager

---
+++ src/main/java/com/orinsoftware/gibbsfangame/KeyboardManager.java
@@ -0,0 +1,29 @@
+package com.orinsoftware.gibbsfangame;
+
+public class KeyboardManager 
+{
+
+	private static KeyboardManager instance;
+	
+	private Directions direction;
+	
+	public enum Directions { UP, DOWN, LEFT, RIGHT, NONE };
+	
+	private KeyboardManager() {
+
+		direction = Directions.NONE;
+	}
+	
+	public void setDirection( Directions direction )
+	{
+		this.direction = direction;
+	}
+	
+	public static KeyboardManager getInstance()
+	{
+		if(instance == null)
+			instance = new KeyboardManager();
+		return instance;
+	}
+
+}
